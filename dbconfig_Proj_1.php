c5ab104ab4942ad5d940058a4c1be466ceaf3f0c
Tyrell <tjwright2023@gmail.com>
2025-03-08 23:29:17 -0500

    added: files
    The only thing I did of note was add the files to GitHub. This is the dbconfig file
    
    Reviewed by: Tyrell W

---
+++ dbconfig_Proj_1.php
@@ -0,0 +1,17 @@
+<?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    
+    function connectDB(){
+        $database = "twrigh21";
+        $user = "twrigh21";
+        $pass = "evergreen";
+        $host = "localhost";
+        try {
+            $db = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+            echo "db success"; 
+        }    # You’ll need to take this echo out when you know it’s working
+        catch (PDOException $e) {echo $e;}
+        return $db; 
+    }
+?>
